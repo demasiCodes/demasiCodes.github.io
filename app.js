3d9c004684045954c94d52fbfea26651e29183be
demasiCodes <112286074+demasiCodes@users.noreply.github.com>
2023-07-10 13:03:46 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var titleElement = document.getElementById('title');
+  var bodyElement = document.getElementById('body');
+
+  // Update the title
+  titleElement.textContent = 'Daniel DeMasi';
+
+  // Update the body
+  bodyElement.textContent = 'Welcome!';
+});
